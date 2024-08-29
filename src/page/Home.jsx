aaa69002d5c3d636c0e3b1d7863644f334c03ede
Jay Suthar <suthajay3635@gmail.com>
2024-08-29 12:24:37 +0530

    feat(app): update

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
   return (
     <MaxWidthWrapper className="max-w-8xl pt-5 px-0 sm:px-0 lg:px-0">
       <div id="home" className="banner py-10">
-        <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="leftSec flex flex-col justify-start text-center lg:text-left flex-1">
+        <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="flex flex-col justify-start text-center lg:text-left flex-1">
             <div className="text-4xl font-semibold mb-4">
               <H3 className="block text-4xl text-zinc-700">BITCOIN MINETRIX</H3>
               <span className="flex text-zinc-700">
@@ -52,20 +52,20 @@ const Home = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col justify-center bg-white shadow-lg rounded-lg overflow-hidden p-9 -py-9">
-            <H4 className="bg-primary    text-white text-center py-4 mb-6 text-xl font-normal rounded-md">
+          <div className="flex flex-col justify-start h-fit  bg-white shadow-lg rounded-lg md:mx-10">
+            <H4 className="bg-primary    text-white text-center py-4 mb-6 text-xl font-normal rounded-t-md ">
               $BTCMTX Claim and Token now LIVE!
             </H4>
-            <P className="text-zinc-700 text-center mb-6">
+            <P className="text-zinc-700 text-center mb-6 px-6">
               You can now claim your $BTCMTX tokens. Plus, stake your tokens to
               earn rewards! Add 0xb11dB272EbfABEdf611a07684e82165dB60dC70E to
               your wallet to see your $BTCMTX.
             </P>
-            <div className="text-center mb-6">
+            <div className="text-center mb-6 px-6">
               <P className="font-semibold">USDT Raised:</P>
               <P className="text-2xl text-zinc-800">$12,357,845.88</P>
             </div>
-            <div className="text-center mb-6">
+            <div className="text-center mb-6 px-6">
               <P className="font-semibold">Your claimable BTCMTX</P>
               <P className="text-xl">= 0</P>
             </div>
@@ -73,7 +73,7 @@ const Home = () => {
             <Link
               to="https://dexscreener.com/ethereum/0x053da4662ab98f78254437580105b37aa999749c"
               target="_blank"
-              className="w-full"
+              className="w-full px-6"
             >
               <Button className=" w-full btn bg-primary hover:bg-primary/90 text-white px-4 py-3 rounded-md flex justify-center items-center mb-4">
                 <img
@@ -85,10 +85,16 @@ const Home = () => {
                 <span>BUY ON DEX SCREENER</span>
               </Button>
             </Link>
-            <Button className="btn bg-primary text-white w-full py-3 rounded-md hover:bg-primary/90">
-              Connect Wallet
-            </Button>
-            <div className="mt-4 text-center flex justify-center gap-2 items-center">
+            <Link
+              to="https://dexscreener.com/ethereum/0x053da4662ab98f78254437580105b37aa999749c"
+              target="_blank"
+              className="w-full px-6"
+            >
+              <Button className=" w-full btn bg-primary hover:bg-primary/90 text-white px-4 py-3 rounded-md flex justify-center items-center text-base mb-4 uppercase">
+                <span> Connect Wallet</span>
+              </Button>
+            </Link>
+            <div className="my-4 text-center flex justify-center gap-2 items-center">
               <P className="text-base font-medium text-zinc-800">Powered by</P>
               <Link
                 to="https://web3paymentsolutions.io/"
